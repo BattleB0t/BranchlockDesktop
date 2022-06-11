bcba74621a7d56711117d1ce7958f0ddde6cd2e9
alexh <alex.hammond003@gmail.com>
2022-06-11 15:07:15 +0100

    Added basic components

--- src/main/java/net/bruhitsalex/branchlockdesktop/ui/MainWindow.java
+++ src/main/java/net/bruhitsalex/branchlockdesktop/ui/MainWindow.java
@@ -1,9 +1,18 @@
 package net.bruhitsalex.branchlockdesktop.ui;
 
+import net.bruhitsalex.branchlockdesktop.ui.processing.ProcessingPane;
+import net.bruhitsalex.branchlockdesktop.ui.settings.ConfigPane;
+import net.bruhitsalex.branchlockdesktop.ui.utils.UIUtils;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
+
+    private JPanel mainPanel;
+    private ProcessingPane proccessingPane;
+    private ConfigPane configPane;
 
     public MainWindow() {
         setTitle("Branchlock Desktop");
@@ -15,8 +24,28 @@ public class MainWindow extends JFrame {
             ex.printStackTrace();
         }
 
+        setSize(new Dimension(1280, 720));
+        setPreferredSize(new Dimension(1280, 720));
+        initComponents();
+
         UIUtils.centreWindow(this);
         pack();
+    }
+
+    private void initComponents() {
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        getContentPane().add(mainPanel);
+
+        JSplitPane splitPane = new JSplitPane();
+        splitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+
+        proccessingPane = new ProcessingPane();
+        splitPane.setLeftComponent(proccessingPane);
+        configPane = new ConfigPane();
+        splitPane.setRightComponent(configPane);
+
+        mainPanel.add(splitPane, BorderLayout.CENTER);
     }
 
 }

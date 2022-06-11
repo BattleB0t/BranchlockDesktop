49aa117bdd79302b4d6a0589af0e260741e22895
alexh <alex.hammond003@gmail.com>
2022-06-12 00:09:22 +0100

    Added addLibrary and getAllLibraries

--- src/main/java/net/bruhitsalex/branchlockdesktop/ui/processing/LibrariesPane.java
+++ src/main/java/net/bruhitsalex/branchlockdesktop/ui/processing/LibrariesPane.java
@@ -1,8 +1,11 @@
 package net.bruhitsalex.branchlockdesktop.ui.processing;
 
 import lombok.Getter;
+import net.bruhitsalex.branchlockdesktop.ui.custom.JarTreeNode;
+import net.bruhitsalex.branchlockdesktop.ui.custom.PathTreeNode;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +13,7 @@ import java.util.List;
 @Getter
 public class LibrariesPane extends JPanel {
 
-    private final List<File> files = new ArrayList<>();
-
+    private final DefaultMutableTreeNode root;
     private final JLabel label;
     private final JTree tree;
     private final JButton add;
@@ -21,7 +23,7 @@ public class LibrariesPane extends JPanel {
         label = new JLabel("Libraries");
         add = new JButton("Add");
         remove = new JButton("Remove");
-        tree = new JTree();
+        tree = new JTree(root = new DefaultMutableTreeNode("0 libraries added"));
         initLayout();
     }
 
@@ -54,6 +56,42 @@ public class LibrariesPane extends JPanel {
         );
 
         setLayout(layout);
+    }
+
+    public void addLibrary(File file) {
+        File parent = file.getParentFile();
+        String name = ".../" + parent.getName();
+
+        DefaultMutableTreeNode newOrFoundChild = new PathTreeNode(parent);
+        boolean wasFound = false;
+        for (int i = 0; i < root.getChildCount(); i++) {
+            PathTreeNode child = (PathTreeNode) root.getChildAt(i);
+            if (name.equals(child.getUserObject())) {
+                newOrFoundChild = child;
+                wasFound = true;
+                break;
+            }
+        }
+
+        if (wasFound) {
+            root.add(newOrFoundChild);
+        }
+
+        newOrFoundChild.add(new JarTreeNode(file));
+    }
+
+    public List<File> getAllLibraries() {
+        List<File> files = new ArrayList<>();
+
+        for (int i = 0; i < root.getChildCount(); i++) {
+            PathTreeNode pathNode = (PathTreeNode) root.getChildAt(i);
+            for (int n = 0; n < pathNode.getChildCount(); n++) {
+                JarTreeNode jarTreeNode = (JarTreeNode) pathNode.getChildAt(n);
+                files.add(jarTreeNode.getJar());
+            }
+        }
+
+        return files;
     }
 
 }

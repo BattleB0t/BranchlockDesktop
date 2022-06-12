3d521040fc2475c71f44478aa2ec9ce2b4e22f05
alexh <alex.hammond003@gmail.com>
2022-06-12 17:15:17 +0100

    Lombok

--- src/main/java/net/bruhitsalex/branchlockdesktop/processing/Processing.java
+++ src/main/java/net/bruhitsalex/branchlockdesktop/processing/Processing.java
@@ -1,5 +1,7 @@
 package net.bruhitsalex.branchlockdesktop.processing;
 
+import lombok.Getter;
+import lombok.Setter;
 import net.bruhitsalex.branchlockdesktop.processing.config.Config;
 import net.bruhitsalex.branchlockdesktop.processing.config.DefaultConfig;
 import net.bruhitsalex.branchlockdesktop.ui.settings.AbstractOptionPanel;
@@ -9,8 +11,8 @@ import java.util.List;
 
 public class Processing {
 
-    public static List<AbstractOptionPanel> optionPanels = new ArrayList<>();
-    public static Config config = new DefaultConfig();
+    @Getter private static List<AbstractOptionPanel> optionPanels = new ArrayList<>();
+    @Getter @Setter private static Config config = new DefaultConfig();
 
     public static void updatePanelOptions() {
         optionPanels.forEach(AbstractOptionPanel::setOptionFromConfig);

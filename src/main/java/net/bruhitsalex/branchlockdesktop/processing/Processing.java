99554b8058e1058bee97a64d4918e122132c955f
alexh <alex.hammond003@gmail.com>
2022-06-12 16:54:43 +0100

    Global config var

---
+++ src/main/java/net/bruhitsalex/branchlockdesktop/processing/Processing.java
@@ -0,0 +1,10 @@
+package net.bruhitsalex.branchlockdesktop.processing;
+
+import net.bruhitsalex.branchlockdesktop.processing.config.Config;
+import net.bruhitsalex.branchlockdesktop.processing.config.DefaultConfig;
+
+public class Processing {
+
+    public static Config config = new DefaultConfig();
+
+}

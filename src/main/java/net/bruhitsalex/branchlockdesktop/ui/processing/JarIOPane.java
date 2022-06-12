285cabc5491c830d5325115b7388548fcd995a60
alexh <alex.hammond003@gmail.com>
2022-06-12 04:10:35 +0100

    Added file choosers for input and output jars

--- src/main/java/net/bruhitsalex/branchlockdesktop/ui/processing/JarIOPane.java
+++ src/main/java/net/bruhitsalex/branchlockdesktop/ui/processing/JarIOPane.java
@@ -1,10 +1,13 @@
 package net.bruhitsalex.branchlockdesktop.ui.processing;
 
+import li.flor.nativejfilechooser.NativeJFileChooser;
 import lombok.Getter;
 import net.bruhitsalex.branchlockdesktop.ui.impl.SwingFileIO;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.File;
 
 @Getter
 public class JarIOPane extends JPanel {
@@ -16,7 +19,31 @@ public class JarIOPane extends JPanel {
         input = new SwingFileIO("Input Jar");
         output = new SwingFileIO("Output Jar");
         setMaximumSize(new Dimension(Short.MAX_VALUE, 28));
+        initActions();
         initLayout();
+    }
+
+    private void initActions() {
+        JFileChooser inputFileChooser = new NativeJFileChooser();
+        inputFileChooser.setFileFilter(new FileNameExtensionFilter("JAR", "*.jar"));
+        inputFileChooser.setMultiSelectionEnabled(false);
+        input.getButton().addActionListener(e -> {
+            int result = inputFileChooser.showDialog(this, "Pick Jar");
+            if (result == JFileChooser.APPROVE_OPTION) {
+                input.getPath().setText(inputFileChooser.getSelectedFile().getAbsolutePath());
+            }
+        });
+
+        JFileChooser outputFileChooser = new NativeJFileChooser();
+        outputFileChooser.setFileFilter(new FileNameExtensionFilter("JAR", "*.jar"));
+        outputFileChooser.setMultiSelectionEnabled(false);
+        outputFileChooser.setDialogType(JFileChooser.SAVE_DIALOG);
+        output.getButton().addActionListener(e -> {
+            int result = outputFileChooser.showSaveDialog(this);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                output.getPath().setText(outputFileChooser.getSelectedFile().getAbsolutePath());
+            }
+        });
     }
 
     private void initLayout() {

235903d1cf355b0e6fdd7c915396ab4a9c1c027d
alexh <alex.hammond003@gmail.com>
2022-07-01 01:08:52 +0100

    Loads scripts from folder

--- src/main/java/net/bruhitsalex/branchlockdesktop/saved/ScriptsManager.java
+++ src/main/java/net/bruhitsalex/branchlockdesktop/saved/ScriptsManager.java
@@ -1,20 +1,28 @@
 package net.bruhitsalex.branchlockdesktop.saved;
 
+import lombok.Getter;
 import net.bruhitsalex.branchlockdesktop.processing.Processing;
 import net.bruhitsalex.branchlockdesktop.processing.config.Config;
 import org.yaml.snakeyaml.Yaml;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScriptsManager {
 
     private static File scriptsFolder;
+    @Getter private static List<Config> allConfigs;
 
     public static void init(File scriptsFolder) {
         ScriptsManager.scriptsFolder = scriptsFolder;
+        ScriptsManager.allConfigs = new ArrayList<>();
         saveDefaultScript();
+        loadScriptsFromFile();
     }
 
     private static void saveDefaultScript() {
@@ -38,6 +46,22 @@ public class ScriptsManager {
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Unable to save script: " + file.getAbsolutePath());
             System.exit(-1);
+        }
+    }
+
+    private static void loadScriptsFromFile() {
+        File[] found = scriptsFolder.listFiles();
+
+        if (found == null || found.length == 0) {
+            return;
+        }
+
+        for (File file : found) {
+            Yaml yaml = new Yaml();
+            try {
+                Config config = yaml.loadAs(new FileReader(file), Config.class);
+                allConfigs.add(config);
+            } catch (FileNotFoundException ignored) {}
         }
     }
 
